2eac04c9d2ad54c75fb6c45934489b70dcc54bad
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-15 07:32:14 +0100

    Opdracht 1 - 1 tot en met 4
    opdracht 1/3 vier van vier afgerondt

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,50 +1,30 @@
-let menuKaart =["Lasagne en macaroni", "Spaghetti met ham en kaas","Macaroni met ham en kaas",
-"Tagliatelli met ham en kaas","Spaghetti met spinazie en room",
-"Macaroni met spinazie en room","Tagliatelli met spinazie en room",
-"Spaghetti met gehakt-tomatensaus en kaas *","Macaroni met gehakt-tomatensaus en kaas *",
-"Tagliatelli met gehakt-tomatensaus en kaas *"];
+let menuKaart = ["Lasagne en macaroni", "Spaghetti met ham en kaas", "Macaroni met ham en kaas",
+  "Tagliatelli met ham en kaas", "Spaghetti met spinazie en room",
+  "Macaroni met spinazie en room", "Tagliatelli met spinazie en room",
+  "Spaghetti met gehakt-tomatensaus en kaas *", "Macaroni met gehakt-tomatensaus en kaas *",
+  "Tagliatelli met gehakt-tomatensaus en kaas *"
+];
 
-//Defineer menuKaart zonder waarde
-let menuKaartKopie2Up =[], menuKaartKopieUn =[];
-
-for (let a=0; a<menuKaart.length; a++)
-{
-menuKaartKopieUn.unshift(menuKaart[a]);
-menuKaartKopie2Up.push(menuKaart[a]);
-}
-/*
-
+//Defineer array zonder waarde
+let menuKaartKopieUn =[],menuKaartKopieUp =[];
 //opdracht 1 - 1/4
-function gerechten01()
-{
-  for(let i=0; i<= menuKaart.length; i++)
-  {
-  console.log(menuKaart[i]);
+function gerechten01() {
+  for (let i = 0; i <= menuKaart.length; i++) {
+    console.log(menuKaart[i]);
   }
 }
 //opdracht 1 - 2/4
 
-function gerechten02(number2)
-{
-return menuKaart[number2];
+function gerechten02(number2) {
+  return menuKaart[number2];
 }
 
-//opdracht 1 - 3/4
+//opdracht 1 - 3/4 en opdracht 4/4
 
-function gerechten03(number3)
-{
-return menuKaart[number3] = menukaartdrie[number3];
+function gerechten03(xy){
+return menuKaartKopieUn = (menuKaart[xy]);
 }
 
-/*
-//opdracht 1 - 4/4
-function gerechten04(number4)
-{
-
+function gerechten031(xy){
+return gerechten03(xy);
 }
-
-function gerechten04(number)
-{
-
-}
-*/
